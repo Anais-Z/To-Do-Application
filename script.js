ce435a8ceba4bbc3a15d5810a01831c1a80ff121
Anais Zulfequar <anaiszed447@gmail.com>
2021-07-27 23:45:34 -0400

    Created the logic that adds a task item when user clicks button.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const submit = document.querySelector(".submit")
 const deleteAll = document.querySelector(".deleteAll")
 
 //creating the object to list the task
-const arrays = []
+const tasks = []
 
 //creating the error message if user doesn't enter task
 const error = document.createElement("p")
@@ -21,15 +21,42 @@ const error = document.createElement("p")
 //creating the var to access to div to place error message
 const errorSection = document.querySelector(".error-section")
 
-submit.addEventListener("click", e =>{
+//creating a var to represent the index of the array
+let arrayIndex = 0;
+ 
+
+//accessing the un ordered list element
+const orderedList = document.querySelector(".ordered-list")
+
+submit.addEventListener("click", () =>{
  
     if(enterTask.value == ""){
         error.innerText = "Enter a task"
         error.style.color = "red"
         return errorSection.append(error)
+    }else{
+        setTask(enterTask.value)
+      arrayIndex =  incrementIndex(arrayIndex)
     }
 })
 
 deleteAll.addEventListener("click", e =>{
 
 })
+
+//creating a function which will place the task in an object
+const setTask = (task) =>{
+    //creating a list item element
+    const listItem = document.createElement("li");
+        taskItem = {
+            "specifiedTask": task
+        }
+        tasks.push(taskItem)
+        listItem.innerText = tasks[arrayIndex].specifiedTask
+        return orderedList.append( listItem)
+         
+}
+
+const incrementIndex = (arrayIndex) =>{
+    return ++arrayIndex
+}
